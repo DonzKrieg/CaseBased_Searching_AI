ac87664b8b1675832ca7b40ac8fba6c8b5292ae9
DonzKrieg <iromdhon2004@gmail.com>
2025-04-17 13:33:36 +0700

    menambahkan fungsi decode kromosom

--- populasi.py
+++ populasi.py
@@ -10,3 +10,15 @@ def generate_population():
         chromosome = ''.join(random.choice(['0', '1']) for _ in range(CHROM_LENGTH * 2))
         population.append(chromosome)
     return population
+
+def decode_gene(gene):
+    decimal = int(gene, 2)
+    max_decimal = 2**CHROM_LENGTH - 1
+    return X_MIN + (decimal / max_decimal) * (X_MAX - X_MIN)
+
+def decode_chromosome(chromosome):
+    gene1 = chromosome[:CHROM_LENGTH]
+    gene2 = chromosome[CHROM_LENGTH:]
+    x1 = decode_gene(gene1)
+    x2 = decode_gene(gene2)
+    return x1, x2
